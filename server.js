6820ab7d101bb22b2b7b3f05355005101296a90a
BPapin19 <bmpapin@yahoo.com>
2021-09-16 23:56:47 -0700

    Version 1.0.0

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+    });
 }
 
 app.listen(process.env.PORT || apiPort, () => console.log(`Server running on port ${apiPort}`));
