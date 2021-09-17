35431b5d7cf64c0414e1c5400e0083c5f78a9140
BPapin19 <bmpapin@yahoo.com>
2021-09-17 02:50:01 -0700

    Version 1.0.0

--- controllers/file-ctrl.js
+++ controllers/file-ctrl.js
@@ -58,7 +58,7 @@ const file = new File();
 uploadFile = (req, res) => {
     upload(req, res, () => {
        file.meta_data = req.file;
-    }).catch((err) => {console.log(err)});
+    });
 
 file
     .save()
