d0a3680db5e0e28aa4ecacf454a0845e26f76d5b
BPapin19 <bmpapin@yahoo.com>
2021-09-16 23:22:15 -0700

    Version 1.0.0

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { React, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import AddSpot from "./components/AddSpot";
+import FindSpot from "./components/FindSpot";
+import NavBar from "./components/NavBar";
+import Signup from "./components/SignUp";
+import Login from "./components/Login";
+import UpdateProfile from "./components/UpdateProfile";
+import MySpots from "./components/MySpots";
+import ForgotPassword from "./components/ForgotPassword";
+import PrivateRoute from "./components/PrivateRoute";
+import { AuthProvider} from "./contexts/AuthContext";
+
+export default function App() {
+  const [shouldUpdate, setShouldUpdate] = useState(false);
+
+  return (
+    <>
+    <AuthProvider>
+      <BrowserRouter>
+      <NavBar {...{shouldUpdate}}/>
+        <Switch>
+          <Route component={Home} path='/' exact/>
+          <PrivateRoute component={AddSpot} path='/add-spot' />
+          <Route component={FindSpot} path='/find-spot' />
+          <PrivateRoute component={() => <Profile {...{setShouldUpdate}} />} path='/profile' />
+          <PrivateRoute component={UpdateProfile} path='/update-profile' />
+          <PrivateRoute component={MySpots} path='/my-spots' />
+          <Route component={Signup} path='/signup' />
+          <Route component={Login} path='/login' />
+          <Route component={ForgotPassword} path='/forgot-password' />
+        </Switch>
+      </BrowserRouter>
+      </AuthProvider>
+  </>
+  );
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./db/db');
+const path = require('path');
+
+const spotRouter = require('./routes/spot-router');
+
+const app = express();
+const apiPort = 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use('/api', spotRouter);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
